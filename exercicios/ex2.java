464d805514784df4bd93476850965dafbf3aeb2e
FMX <felipemxavier@hotmail.com.br>
2025-10-19 14:40:09 -0300

    soma, 3 dígitos

--- exercicios/ex2.java
+++ exercicios/ex2.java
@@ -1,18 +1,25 @@
 package exercicios;
 import java.util.Scanner;
 
+// Crie um programa que calcule e mostre a soma de três números fornecidos pelo usuário
+
 public class ex2 {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    
+    // entrada de dados
+    Scanner sc = new Scanner(System.in); 
+
+     // variáveis 
     int a, b, c, soma;
 
+    // solicitação de dados com quabra de linha 
     a = sc.nextInt(); 
     b = sc.nextInt();
     c = sc.nextInt();
-    
-    soma = a + b + c;
 
+    // soma
+    soma = a + b + c;
+    
+    // resultado com mensagem
     System.out.println("Calculando os dados: " + soma);
 
     sc.close();
